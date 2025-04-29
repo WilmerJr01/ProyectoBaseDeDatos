0dd8d5cde2927c8294de2391369abc9c3137bd7f
Wilmer Junior Santiago Donado <jwsantiago@uninorte.edu.co>
2025-04-29 13:16:00 -0500

    ROUTES
    Reformulación del sistema de rutas

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,49 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
 import "../styles/App.css";
+
+//Componentes 
 import Header from "./header.jsx";
+import Inicio from "./inicio.jsx"; 
 import Partidos from "./partidos.jsx";
 import Competencias from "./competencias.jsx";
 import Equipos from "./equipos.jsx";
 import SobreNosotros from "./SobreNosotros.jsx"; // ➡️ Agregado aquí
-import axios from "axios";
-import { formDate } from "../functions/functions.js";
 
 function App() {
-  const [partidos, setPartidos] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:3000/database/last')
-    .then((response) => {
-      setPartidos(response.data);
-    })
-    .catch((error) => {
-      console.error('Error al obtener los partidos:', error);
-    })
-  }, []);
-
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={
-          <main>
-            <div className="hero">
-              <h1>Bienvenido a la App de Fútbol</h1>
-              <p>Tu fuente de información sobre competiciones y partidos en directo.</p>
-            </div>
-            <div className="content">
-              <h2>Últimos Partidos</h2>
-              <ul className="match-list">
-                {partidos.map((partido, index) => (
-                  <li key={index} className="match-item">
-                    <p>Fecha {formDate(partido.date)}</p>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </main>
-        } />
+        <Route path="/" element={ <Inicio /> } />
         <Route path="/partidos" element={<Partidos />} />
         <Route path="/competencias" element={<Competencias />} />
         <Route path="/equipos" element={<Equipos />} />
---
+++ frontend/components/inicio.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { formDate } from "../functions/functions.js";
+
+function Inicio() {
+    const [partidos, setPartidos] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/database/last")
+            .then((response) => {
+                setPartidos(response.data);
+            })
+            .catch((error) => {
+                console.error("Error al obtener los partidos:", error);
+            });
+    }, []);
+
+    return (
+        <main>
+            <div className="hero">
+                <h1>Bienvenido a la App de Fútbol</h1>
+                <p>
+                    Tu fuente de información sobre competiciones y partidos en
+                    directo.
+                </p>
+            </div>
+            <div className="content">
+                <h2>Últimos Partidos</h2>
+                <ul className="match-list">
+                    {partidos.map((partido, index) => (
+                        <li key={index} className="match-item">
+                            <p>Fecha {formDate(partido.date)}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </main>
+    );
+}
+
+export default Inicio;
